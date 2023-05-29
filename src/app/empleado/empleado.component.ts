d1189fbd011ad8fb4a32856fbd4c459e5ab605b5
Agustin Diez <agustindxm@gmail.com>
2023-05-29 09:32:56 -0300

    adding subjects

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css'],
 })
-export class EmpleadoComponent implements OnInit {
+export class EmpleadoComponent implements OnInit, OnDestroy {
+  private saludo$ = new Subject<string>();
+  private borrar$ = new Subject<number>();
+  // private saludo2$ = new BehaviorSubject<string>('');
+
   empleados: Empleado[] = [];
   julia: Empleado = {
     nombre: 'Julia',
@@ -19,14 +24,37 @@ export class EmpleadoComponent implements OnInit {
     apellido: 'Diaz',
     edad: 19,
     empresa: 'Dedalus',
-  }
-  
+  };
+
+  agus: Empleado = {
+    nombre: 'Agustin',
+    apellido: 'Diez',
+    edad: 29,
+    empresa: 'Dedalus',
+  };
+
   habilitacionCuadro: boolean = false;
 
   constructor() {}
 
+
+  ngOnDestroy(): void {
+    this.borrar$.unsubscribe()
+    this.saludo$.unsubscribe()
+  }
+
   ngOnInit(): void {
-    this.empleados = [this.julia, this.jose];
+    this.empleados = [this.julia, this.jose, this.agus];
+    this.saludo$.subscribe((e) => console.log(e));
+    this.borrar$.subscribe((id) => this.empleados.splice(id, 1));
+  }
+
+  saludar(saludo: string) {
+    this.saludo$.next(saludo);
+  }
+
+  borrarUsuario(id: number) {
+    this.borrar$.next(id);
   }
 
   getEdad(idEmpleado: Empleado['edad']) {
